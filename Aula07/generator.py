5bf73b360174889331afc2c97bd27751837f7b4d
João Pedro De Munno Durante <jaodurante@gmail.com>
2021-01-22 20:13:00 -0300

    aula07 started

--- Aula07/generator.py
+++ Aula07/generator.py
@@ -10,19 +10,36 @@ def generate(a, m, n, init, c):
         result.append(item / m)
         init = item
 
-    result.pop(0)
-    return result
+    return result[0]
 
+def drunk_simulator():
+    t = 0
+    x = 0
+    y = 0
+    init = 2
+
+
+    while((x != 3 and y != 4) or t != 60):
+        rand1 = generate(16807, pow(2, 31) - 1, init, 1000, 0)
+        rand2 = generate(16807, pow(2, 31) - 1, init+2, 1000, 0)
+
+        if rand1 <= 0.3:
+            t += 1
+
+        if rand2 <= 0.35:
+            x += 1
+        elif 0.35 < rand2 and rand2 <= 0.8:
+            y += 1
+        elif 0.8 < rand2 and rand2 <= 0.9:
+            x += 1
+        else:
+            y -= 1
+        
+        t += 5
+
+    print(x)
+    print(y)
+    print(t)
 
 if __name__ == "__main__":
-    a = 16807
-    c = 0
-    m = pow(2, 31) - 1
-    # init = int(input('Insira o valor inicial: '))
-    # n = int(input('Insira o valor de n: '))
-    init = 2
-    n = 1000
-
-    x = generate(a, m, n, init, c)
-    y = generate(a, m, n, init+1, c)
-    
+    drunk_simulator()
