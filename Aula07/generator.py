a0b0e5f7aad3ac1858b7464b5695b3106bcb86e4
João Pedro De Munno Durante <jaodurante@gmail.com>
2021-01-25 21:44:06 -0300

    Aula07 completed

--- Aula07/generator.py
+++ Aula07/generator.py
@@ -14,22 +14,21 @@ def generate(a, m, n, init, c):
     return result
 
 def drunk_simulator():
+    a = 2000
+    m = pow(2, 12) - 1
+    n = 10000
+    init = 1
+    c = 2
+
     t = 0
     x = 0
     y = 0
     i = 0
 
-    rand1 = generate(16807, pow(2, 31) - 1, 5000, 2, 0)
-    rand2 = generate(16807, pow(2, 31) - 1, 5000, 3, 0)
-    print(rand1)
-    print(rand2)
+    rand1 = generate(a, m, n, init, c)
+    rand2 = generate(a, m, n, init+1, c)
 
-    while((x != 3 and y != 4) or t != 60):
-        if i == 4999:
-            rand1 = generate(16807, pow(2, 31) - 1, 5000, 2, 0)
-            rand2 = generate(16807, pow(2, 31) - 1, 5000, 3, 1)
-            i = 0
-
+    while((x != 3 or y != 4) and t <= 360):
         if rand1[i] <= 0.3:
             t += 1
 
@@ -39,16 +38,15 @@ def drunk_simulator():
             y += 1
         elif 0.8 < rand2[i] and rand2[i] <= 0.9:
             x -= 1
-        else:
+        elif rand2[i] > 0.9:
             y -= 1
         
         t += 5
         i += 1
 
-        # print("---")
-        # print(x)
-        # print(y)
-        # print(t)
+    print(x)
+    print(y)
+    print(t)
 
 if __name__ == "__main__":
     drunk_simulator()
