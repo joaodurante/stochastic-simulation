ee21cb8a8efb5d0725d00ef104b78bf5785c134e
João Pedro De Munno Durante <jaodurante@gmail.com>
2021-02-02 20:15:38 -0300

    bank_queue started

--- Trabalho/generator.py
+++ Trabalho/generator.py
@@ -11,7 +11,61 @@ def generate(a, m, n, init, c):
         init = item
 
     result.pop(0)
-    print(result[0:5]) 
+    return result[0:5]
+
+def get_tec(num):
+    return (-numpy.log(num) * 15)
+
+def get_ts(num):
+    if num <= 0.6:
+        return (-numpy.log(num) * 15)
+    elif num > 0.6 and num <= 0.9:
+        return ((-numpy.log(num) * 45) + 30)
+    elif num > 0.9:
+        return ((-numpy.log(num) * 180) + 60)
+
+def get_arrival_time(last_row, tec):
+    if last_row is not None:
+        return last_row['arrival_time'] + tec
+    else:
+        return tec
+
+def get_start_time(last_row, arrival_time):
+    if last_row is not None:
+        return max(last_row['end_time'], arrival_time)
+    else:
+        return arrival_time
+
+def get_end_time(ts, start_time):
+    return ts + start_time
+
+def get_queue_time(arrival_time, start_time):
+    return start_time - arrival_time
+
+def get_system_time(ts, queue_time):
+    return ts + queue_time
+
+def get_free_time(last_row, arrival_time):
+    if last_row is not None:
+        diff = arrival_time - last_row['end_time']
+        return max(0, diff)
+    else:
+        return arrival_time
+
+def bank_queue(nums):
+    rows = []
+
+    for i in nums:
+        last_row = rows[len(rows) - 1] if len(rows) > 0 else None
+
+        tec = get_tec(i)
+        ts = get_ts(i)
+        arrival_time = get_arrival_time(last_row, tec)
+        start_time = get_start_time(last_row, arrival_time)
+        end_time = get_end_time(ts, start_time)
+        queue_time = get_queue_time(arrival_time, start_time)
+        system_time = get_system_time(ts, queue_time)
+        free_time = get_free_time(last_row, arrival_time)
     
 
 if __name__ == "__main__":
@@ -20,4 +74,4 @@ if __name__ == "__main__":
     m = pow(2, 31) - 1
     init = 2
     n = 1000
-    generate(a, m, n, init, c)
+    result = generate(a, m, n, init, c)
