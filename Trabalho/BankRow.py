3e530cc2851ae2d30d9607d4e2ff44ab784eb83f
João Pedro De Munno Durante <jaodurante@gmail.com>
2021-02-02 20:56:39 -0300

    code refactoring

---
+++ Trabalho/BankRow.py
@@ -0,0 +1,53 @@
+import numpy
+
+class BankRow:
+    def __init__(self, random, last_row):
+        self.last_row = last_row
+        self.tec = self.get_tec(random)
+        self.ts = self.get_ts(random)
+        self.arrival_time = self.get_arrival_time()
+        self.start_time = self.get_start_time()
+        self.end_time = self.get_end_time()
+        self.queue_time = self.get_queue_time()
+        self.system_time = self.get_system_time()
+        self.free_time = self.get_free_time()
+
+    def get_tec(self, num):
+        return (-numpy.log(num) * 15)
+
+    def get_ts(self, num):
+        if num <= 0.6:
+            return ((-numpy.log(num) * 15) + 15)
+        elif num > 0.6 and num <= 0.9:
+            return ((-numpy.log(num) * 45) + 30)
+        elif num > 0.9:
+            return ((-numpy.log(num) * 180) + 60)
+
+    def get_arrival_time(self):
+        if self.last_row is not None:
+            return self.last_row.arrival_time + self.tec
+        else:
+            return self.tec
+
+    def get_start_time(self):
+        if self.last_row is not None:
+            return max(self.last_row.end_time, self.arrival_time)
+        else:
+            return self.arrival_time
+
+    def get_end_time(self):
+        return self.ts + self.start_time
+
+    def get_queue_time(self):
+        return self.start_time - self.arrival_time
+
+    def get_system_time(self):
+        return self.ts + self.queue_time
+
+    def get_free_time(self):
+        if self.last_row is not None:
+            diff = self.arrival_time - self.last_row.end_time
+            return max(0, diff)
+        else:
+            return self.arrival_time
+
--- Trabalho/generator.py
+++ Trabalho/generator.py
@@ -1,5 +1,6 @@
 import math
 import numpy
+from BankRow import BankRow
 import matplotlib.pyplot as plt
 
 def generate(a, m, n, init, c):
@@ -13,59 +14,16 @@ def generate(a, m, n, init, c):
     result.pop(0)
     return result[0:5]
 
-def get_tec(num):
-    return (-numpy.log(num) * 15)
-
-def get_ts(num):
-    if num <= 0.6:
-        return (-numpy.log(num) * 15)
-    elif num > 0.6 and num <= 0.9:
-        return ((-numpy.log(num) * 45) + 30)
-    elif num > 0.9:
-        return ((-numpy.log(num) * 180) + 60)
-
-def get_arrival_time(last_row, tec):
-    if last_row is not None:
-        return last_row['arrival_time'] + tec
-    else:
-        return tec
-
-def get_start_time(last_row, arrival_time):
-    if last_row is not None:
-        return max(last_row['end_time'], arrival_time)
-    else:
-        return arrival_time
-
-def get_end_time(ts, start_time):
-    return ts + start_time
-
-def get_queue_time(arrival_time, start_time):
-    return start_time - arrival_time
-
-def get_system_time(ts, queue_time):
-    return ts + queue_time
-
-def get_free_time(last_row, arrival_time):
-    if last_row is not None:
-        diff = arrival_time - last_row['end_time']
-        return max(0, diff)
-    else:
-        return arrival_time
-
 def bank_queue(nums):
     rows = []
 
     for i in nums:
         last_row = rows[len(rows) - 1] if len(rows) > 0 else None
+        row = BankRow(i, last_row)
+        rows.append(row)
+        print(vars(row))
 
-        tec = get_tec(i)
-        ts = get_ts(i)
-        arrival_time = get_arrival_time(last_row, tec)
-        start_time = get_start_time(last_row, arrival_time)
-        end_time = get_end_time(ts, start_time)
-        queue_time = get_queue_time(arrival_time, start_time)
-        system_time = get_system_time(ts, queue_time)
-        free_time = get_free_time(last_row, arrival_time)
+    return rows
     
 
 if __name__ == "__main__":
@@ -75,3 +33,4 @@ if __name__ == "__main__":
     init = 2
     n = 1000
     result = generate(a, m, n, init, c)
+    rows = bank_queue(result)
